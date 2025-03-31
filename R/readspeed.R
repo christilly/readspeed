c12e6bfb8ebe3c88a158ee5cbff07171adb51204
christilly <156473204+christilly@users.noreply.github.com>
2025-03-31 12:35:57 -0400

    Processing data
    I processed data to remove books that were not read and assign date formatting to the date added and date read columns.

--- R/readspeed.R
+++ R/readspeed.R
@@ -6,6 +6,20 @@ library("dplyr")
 GR_data <- read.csv("/Users/tillytran/Desktop/goodreads_library_export.csv", header = TRUE)
 
 #Get rid of not readily useful data columns
-GR_data <- GR_data %>% select(-c(Author.l.f, Additional.Authors, Binding, ISBN, ISBN13, Year.Published, Original.Publication.Year, Exclusive.Shelf, My.Review, Spoiler, Private.Notes, Owned.Copies, Bookshelves, Bookshelves.with.positions))
+GR_data <- GR_data %>% select(-c(Author.l.f, Additional.Authors, Binding, ISBN, ISBN13, Year.Published, Original.Publication.Year, Exclusive.Shelf, My.Review, Spoiler, Private.Notes, Owned.Copies, Bookshelves, Bookshelves.with.positions, Publisher))
 
-GR_data
+
+#Convert empty Date.Read dates to NA and remove those books
+GR_clean <- GR_data %>%
+  mutate(Date.Read = na_if(Date.Read, "")) %>%  # Convert "" to NA
+  filter(!is.na(Date.Read)) # Remove rows where Date.Read is NA
+GR_clean
+
+#Convert to date format
+GR_clean$Date.Read <- as.Date(GR_clean$Date.Read, format="%Y/%m/%d")
+GR_clean$Date.Added <- as.Date(GR_clean$Date.Added, format="%Y/%m/%d")
+
+#Calculate number of days between date added and date read
+GR_clean$days_diff <- as.numeric(GR_clean$Date.Read - GR_clean$Date.Added)
+
+
